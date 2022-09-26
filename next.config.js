abb5254e58b359b22990a048935725842949ef27
shabbir-ahmed <9017121+shabbir-ahmed@users.noreply.github.com>
2022-09-26 11:10:19 +0600

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	env: {
-		OPENAI_API_KEY: "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",
+		OPENAI_API_KEY: "sk-",
 	},
 };
 
